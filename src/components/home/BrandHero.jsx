94413dca6420b05a7b002162ae48b03f0e310318
nmbhseluna <nmbhseluna@gmail.com>
2026-07-21 19:46:54 +0000

    File changes

--- src/components/home/BrandHero.jsx
+++ src/components/home/BrandHero.jsx
@@ -1,13 +1,10 @@
-import { Image } from '@/components/ui/image';
-
 export default function BrandHero() {
   return (
     <div className="w-full bg-background pt-12">
-      <Image
+      <img
         src="https://media.base44.com/images/public/6a5f78595b9383901d7402b6/73f121e91_IMG_0471.png"
         alt="Seluna"
-        fittingType="fit"
-        className="w-full"
+        className="w-full h-auto block"
       />
     </div>
   );
